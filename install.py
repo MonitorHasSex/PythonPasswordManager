d2a8c7ddc1a8044aa756e0f3be71ac24f304b5da
Cherishment <52844366+Cherishment@users.noreply.github.com>
2020-10-09 17:48:59 -0700

    Delete install.py

--- install.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-
-r =requests.get('https://pastebin.com/raw/6rpaMNEM')
-file = open('PPM.py', 'w')
-file.write(r)
-file.close()
-
-print('Install Complete, you may close this terminal window now.')
